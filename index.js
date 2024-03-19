bcbbbd580ce2d9e932fad2535bff9767502cf3d6
BensonGitau <bensonkamaugitau@gmail.com>
2024-03-19 15:36:59 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//1) contains "Welcome, "
+//2) contains the first initial of the name stored in the 'currentUser' variable
+//3) ends with an exclamation point
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
